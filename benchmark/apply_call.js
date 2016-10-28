3488d276823e9f5fba4d203a9b02b9da636048a4
hardog <peng.zhao@husor.com.cn>
2016-10-28 09:52:08 +0800

    change order

--- benchmark/apply_call.js
+++ benchmark/apply_call.js
@@ -10,28 +10,28 @@ var fn3 = function(a, b, c){
 };
 
 suite('apply_call', function () {
-  bench('call - 1 args', function () {
-    fn1.call(null, 1);
-  })
-
   bench('apply - 1 args', function () {
     fn1.apply(null, [1]);
   })
 
-  bench('call - 2 args', function () {
-    fn2.call(null, 1, 2);
+  bench('call - 1 args', function () {
+    fn1.call(null, 1);
   })
 
   bench('apply - 2 args', function () {
     fn2.apply(null, [1, 2]);
   })
 
-  bench('call - 3 args', function () {
-    fn3.call(null, 1, 2, 3);
+  bench('call - 2 args', function () {
+    fn2.call(null, 1, 2);
   })
 
   bench('apply - 3 args', function () {
     fn3.apply(null, [1, 2, 3]);
+  })
+
+  bench('call - 3 args', function () {
+    fn3.call(null, 1, 2, 3);
   })
 })
 
